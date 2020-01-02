c0b79cf4c8d7d48fb4312de9295bd290f1bfe835
junketdev <junketdev@users.noreply.github.com>
2020-01-02 11:59:35 +0530

    LAUNCHER IMAGE[URVISH]

--- app/src/main/java/com/test/junket/SplashscreenActivity.java
+++ app/src/main/java/com/test/junket/SplashscreenActivity.java
@@ -1,5 +1,6 @@
 package com.test.junket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,25 @@ public class SplashscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),LoginActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
 }
