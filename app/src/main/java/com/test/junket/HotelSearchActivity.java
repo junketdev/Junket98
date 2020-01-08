d1c8085642e32252898d1b8542bcd987a0cd4741
junketdev <junketdev@users.noreply.github.com>
2020-01-08 12:24:29 +0530

    Hotel Search activity and details [Nirmal]

--- app/src/main/java/com/test/junket/HotelSearchActivity.java
+++ app/src/main/java/com/test/junket/HotelSearchActivity.java
@@ -1,7 +1,11 @@
 package com.test.junket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HotelSearchActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class HotelSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel_search);
+
+        TextView search = (TextView) findViewById(R.id.btn_search);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHotelDetails(v);
+            }
+        });
+    }
+
+    private void showHotelDetails(View v) {
+        Intent i = new Intent(this, HotelviewActivity.class);
+        startActivity(i);
+
     }
 }
