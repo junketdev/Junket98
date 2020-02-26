a6aed93a3e1c7245762ea7f4e9aa7b7c06ae31fc
junketdev <junketdev@users.noreply.github.com>
2020-02-26 10:41:29 +0530

    Payment Failed Screen [URVISH]

--- app/src/main/java/com/test/junket/PaymentFailed.java
+++ app/src/main/java/com/test/junket/PaymentFailed.java
@@ -3,11 +3,11 @@ package com.test.junket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PaymentConfirmation extends AppCompatActivity {
+public class PaymentFailed extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_successfull);
+        setContentView(R.layout.activity_payment_failed);
     }
 }
